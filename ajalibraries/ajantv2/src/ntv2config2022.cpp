a38e794507057218cd2a31842c631105a84a52e9
davidc <davidc@0f67835d-f0cd-4aaa-b010-e50d4469497b>
2016-11-18 00:36:05 +0000

    set IP address for localIP not for masterIP
    git-svn-id: http://svn.aja.com/svn/ntv2/trunk@282 0f67835d-f0cd-4aaa-b010-e50d4469497b

--- ajalibraries/ajantv2/src/ntv2config2022.cpp
+++ ajalibraries/ajantv2/src/ntv2config2022.cpp
@@ -1303,7 +1303,7 @@ bool  CNTV2Config2022::ConfigurePTP (eSFP port, string localIPAddress)
 
     WriteChannelRegister(kRegPll_PTP_EventUdp   + SAREK_PLL, 0x0140013f);
     WriteChannelRegister(kRegPll_PTP_MstrMcast  + SAREK_PLL, 0xe0000181);
-    WriteChannelRegister(kRegPll_PTP_MstrIP     + SAREK_PLL, addr);
+    WriteChannelRegister(kRegPll_PTP_LclIP      + SAREK_PLL, addr);
 
     WriteChannelRegister(kRegPll_PTP_LclClkIdLo + SAREK_PLL, (0xfe << 24) | ((macHi & 0x000000ff) << 16) | (macLo >> 16));
     WriteChannelRegister(kRegPll_PTP_LclClkIdHi + SAREK_PLL, (macHi & 0xffffff00) | 0xff);
