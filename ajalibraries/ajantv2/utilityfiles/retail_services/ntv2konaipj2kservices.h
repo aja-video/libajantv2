5fa757fcc6d25a1a8cb866ff170e83bb8c5dcf46
Phil Montoya <philm@aja.com>
2017-03-08 10:36:28 -0800

    Turn on service config for encoder and decoder

--- ajalibraries/ajantv2/utilityfiles/retail_services/ntv2konaipj2kservices.h
+++ ajalibraries/ajantv2/utilityfiles/retail_services/ntv2konaipj2kservices.h
@@ -26,14 +26,16 @@ public:
 	virtual void SetDeviceMiscRegisters (NTV2Mode mode);
 
 protected:
-    void   setNetConfig(eSFP  port);
-	void   setRxConfig(NTV2Channel channel);
-	void   setTxConfig(NTV2Channel channel);
+    void	setNetConfig(eSFP  port);
+	void	setRxConfig(NTV2Channel channel);
+	void	setTxConfig(NTV2Channel channel);
 	
-	bool   notEqualPrimary(const rx_2022_channel & hw_channel, const rx2022Config & virtual_config);
-	bool   notEqualPrimary(const tx_2022_channel & hw_channel, const tx2022Config & virtual_config);
-	bool   notEqualMAC(uint32_t lo, uint32_t hi, const MACAddr & macaddr);
-	
+	bool	notEqualPrimary(const rx_2022_channel & hw_channel, const rx2022Config & virtual_config);
+	bool	notEqualPrimary(const tx_2022_channel & hw_channel, const tx2022Config & virtual_config);
+	bool	notEqualMAC(uint32_t lo, uint32_t hi, const MACAddr & macaddr);
+	void	printEncoderConfig(j2kEncoderConfig modelConfig, j2kEncoderConfig encoderConfig);
+	void	printDecoderConfig(j2kDecoderConfig modelConfig, j2kDecoderConfig encoderConfig);
+
 	CNTV2Config2022     * target;
 };
 
