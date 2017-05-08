a509cf05857becd55027f8077550541faaddf128
mrbill <bills@aja.com>
2017-05-08 11:37:56 -0700

    minor cleanup

--- ajalibraries/ajantv2/src/ntv2card.cpp
+++ ajalibraries/ajantv2/src/ntv2card.cpp
@@ -795,6 +795,7 @@ ostream &	operator << (ostream & inOutStr, const NTV2DIDSet & inDIDs)
 }
 
 
+//										SDI Spigot:		   1	   2	   3	   4	   5	   6	   7	   8
 static const ULWord	sFirstIgnoreDIDRegNums[]	=	{	4108,	4172,	4236,	4300,	4364,	4428,	4492,	4556,	0};
 static const ULWord	sLastIgnoreDIDRegNums[]		=	{	4112,	4176,	4240,	4304,	4368,	4432,	4496,	4560,	0};
 static const ULWord	kNumIgnoreDIDRegisters			(sLastIgnoreDIDRegNums[0] - sFirstIgnoreDIDRegNums[0] + 1);
