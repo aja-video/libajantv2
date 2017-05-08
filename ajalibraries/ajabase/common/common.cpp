e85495383e822fa1eec57ce08b7e64d70c42a6be
Sean Long <seanl@aja.com>
2017-05-08 16:45:23 -0700

    Moving common std::string helper functions into ajabase

--- ajalibraries/ajabase/common/common.cpp
+++ ajalibraries/ajabase/common/common.cpp
@@ -6,12 +6,119 @@
 
 #include "common.h"
 
+#include <algorithm>
+#include <sstream>
 #include <string>
 
 std::string& replace(std::string& str, const std::string& from, const std::string& to)
 {
     if (!from.empty())
+    {
         for (size_t pos = 0; (pos = str.find(from, pos)) != std::string::npos; pos += to.size())
+        {
             str.replace(pos, from.size(), to);
+        }
+    }
     return str;
+}
+
+int string_to_int(const std::string str, int fallback)
+{
+    int number = fallback;
+    if (str.length() > 1 &&
+        str[0] == '0' && (str[1] == 'x' || str[1] == 'X'))
+    {
+        const std::string  hexStr(str.substr(2));
+        std::istringstream iss(hexStr);
+        iss >> std::hex >> number;
+        if (iss.fail())
+        {
+            number = fallback;
+        }
+    }
+    else
+    {
+        std::istringstream iss(str);
+        iss >> number;
+        if (iss.fail())
+        {
+            number = fallback;
+        }
+    }
+    return number;
+}
+
+std::string int_to_string(int i)
+{
+    std::ostringstream oss;
+    oss << i;
+    return oss.str();
+}
+
+void split(const std::string& str, const char delim, std::vector<std::string>& elems)
+{
+    std::stringstream ss(str);
+    std::string item;
+    while(std::getline(ss, item, delim))
+    {
+        elems.push_back(item);
+    }
+}
+
+std::vector<std::string> split(const std::string& str, const char delim)
+{
+    std::vector<std::string> elems;
+    split(str, delim, elems);
+    return elems;
+}
+
+std::string& lower(std::string& str)
+{
+    std::transform (str.begin (), str.end (), str.begin (), ::tolower);
+    return str;
+}
+
+std::string& upper(std::string& str)
+{
+    std::transform (str.begin (), str.end (), str.begin (), ::toupper);
+    return str;
+}
+
+std::string& lstrip(std::string& str, const std::string ws)
+{
+    str.erase(0, str.find_first_not_of(ws));
+    return str;
+}
+
+std::string& rstrip(std::string& str, const std::string ws)
+{
+    if(str.length() > 0)
+    {
+        str.erase(str.find_last_not_of(ws)+1,str.length()-1);
+    }
+    return str;
+}
+
+std::string& strip(std::string& str, const std::string ws)
+{
+    lstrip(str,ws);
+    rstrip(str,ws);
+    return str;
+}
+
+std::string join(std::vector<std::string> parts, const std::string delim)
+{
+    std::ostringstream oss;
+    std::vector<std::string>::iterator it;
+    int count=0;
+    for(it=parts.begin();it!= parts.end();++it)
+    {
+        if (count==0)
+            oss << *it;
+        else
+            oss << delim << *it;
+
+        count++;
+    }
+    return oss.str();
 }
--- ajalibraries/ajabase/common/common.h
+++ ajalibraries/ajabase/common/common.h
@@ -15,14 +15,104 @@
 #include "ajabase/common/public.h"
 #include "ajabase/system/debug.h"
 
+static const std::string AJA_WHITESPACE(" \t\n\r");
+
 /**
  *	Replace a substring within a string with a new string
  *
- *  @param[in]  str  The string to modify
- *  @param[in]  from The substring to look for
- *  @param[in]  to   The string to replace the substring with
- *	@return		Reference to the modified STL string.
+ *  @param[in,out]  str  The string to modify
+ *  @param[in]      from The substring to look for
+ *  @param[in]      to   The string to replace the substring with
+ *	@return		    Reference to the modified STL string
  */
 AJA_EXPORT std::string& replace(std::string& str, const std::string& from, const std::string& to);
+
+/**
+ *	Convert the string to an int if possible
+ *
+ *  @param[in]  str      The string to get the int value of
+ *  @param[in]  fallback A default value to return in the case of a failure
+ *	@return		The int value of the input str or fallback in the case of failure
+ */
+AJA_EXPORT int string_to_int(const std::string str, int fallback = 0);
+
+/**
+ *	Place an int into a string
+ *
+ *  @param[in]  i The int to convert to a string
+ *	@return		A string representing the passed int
+ */
+AJA_EXPORT std::string int_to_string(int i);
+
+/**
+ *	Splits a string into substrings at a character delimeter
+ *
+ *  @param[in]   str   The string to split into parts
+ *  @param[in]   delim The character delimeter to split the string at
+ *  @param[out]  elems A vector of strings that contains all the substrings
+ *	@return		 void
+ */
+AJA_EXPORT void split(const std::string& str, const char delim, std::vector<std::string>& elems);
+
+/**
+ *	Splits a string into substrings at a character delimeter
+ *
+ *  @param[in]  str   The string to split into parts
+ *  @param[in]  delim The character delimeter to split the string at
+ *	@return		A vector of strings that contains all the substrings
+ */
+AJA_EXPORT std::vector<std::string> split(const std::string& str, const char delim);
+
+/**
+ *	Converts the passed string to lowercase
+ *
+ *  @param[in,out]  str   The string to make lowercase
+ *	@return		    Reference to the modified STL string
+ */
+AJA_EXPORT std::string& lower(std::string& str);
+
+/**
+ *	Converts the passed string to uppercase
+ *
+ *  @param[in,out]  str   The string to make uppercase
+ *	@return		    Reference to the modified STL string
+ */
+AJA_EXPORT std::string& upper(std::string& str);
+
+/**
+ *	Strips the leading whitespace characters from the string
+ *
+ *  @param[in,out]  str  The string to strip leading characters from
+ *  @param[in]      ws   The whitespace characters to strip
+ *	@return		    Reference to the modified STL string
+ */
+AJA_EXPORT std::string& lstrip(std::string& str, const std::string ws=AJA_WHITESPACE);
+
+/**
+ *	Strips the trailing whitespace characters from the string
+ *
+ *  @param[in,out]  str  The string to strip trailing characters from
+ *  @param[in]      ws   The whitespace characters to strip
+ *	@return		    Reference to the modified STL string
+ */
+AJA_EXPORT std::string& rstrip(std::string& str, const std::string ws=AJA_WHITESPACE);
+
+/**
+ *	Strips the leading & trailing whitespace characters from the string
+ *
+ *  @param[in,out]  str  The string to strip leading & trailing characters from
+ *  @param[in]      ws   The whitespace characters to strip
+ *	@return		    Reference to the modified STL string
+ */
+AJA_EXPORT std::string& strip(std::string& str, const std::string ws=AJA_WHITESPACE);
+
+/**
+ *	Join a vector of strings separated by a string delimeter
+ *
+ *  @param[in]  parts  The vector of strings to join together
+ *  @param[in]  delim  The string delimeter that will separate the strings
+ *	@return		The joined string made up of the parts concatinated with delimeter string
+ */
+AJA_EXPORT std::string join(std::vector<std::string> parts, const std::string delim=" ");
 
 #endif	//	AJA_COMMON_H
