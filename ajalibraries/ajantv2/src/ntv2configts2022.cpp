b8a44361d94aceec1599b7402ac1de67391d9e00
Phil Montoya <philm@aja.com>
2017-05-09 15:51:47 -0700

    Add back in delays to see if this fixes i format flashes

--- ajalibraries/ajantv2/src/ntv2configts2022.cpp
+++ ajalibraries/ajantv2/src/ntv2configts2022.cpp
@@ -33,7 +33,7 @@ CNTV2ConfigTs2022::CNTV2ConfigTs2022(CNTV2Card & device) : CNTV2MBController(dev
 
 bool CNTV2ConfigTs2022::SetupJ2KEncoder(const NTV2Channel channel, const j2kEncoderConfig &config)
 {
-#define WAIT_RESET_MS   600
+#define WAIT_RESET_MS   800
 
     kipdprintf("CNTV2ConfigTs2022::SetupJ2KEncoder channel = %d\n", channel);
 
@@ -89,8 +89,22 @@ bool CNTV2ConfigTs2022::SetupJ2KEncoder(const NTV2Channel channel, const j2kEnco
         }
     }
 
+    // Wait
+    #if defined(AJAWindows) || defined(MSWindows)
+        ::Sleep (WAIT_RESET_MS/2);
+    #else
+        usleep (WAIT_RESET_MS/2 * 1000);
+    #endif
+
     // Disable encoder inputs
     SetEncoderInputEnable( channel, false, false );
+
+    // Wait
+    #if defined(AJAWindows) || defined(MSWindows)
+        ::Sleep (WAIT_RESET_MS/2);
+    #else
+        usleep (WAIT_RESET_MS/2 * 1000);
+    #endif
 
     // Assert reset
 	SetEncoderReset( channel, true );
