ac7b2be5a3284d3b0cd30773ebac90dad8e27b42
mrbill <bills@aja.com>
2017-07-19 17:24:19 -0700

    fixed NTV2DeviceGetAudioMixerSystem to work with both Io4K & Plus

--- ajalibraries/ajantv2/src/ntv2devicefeatures.cpp
+++ ajalibraries/ajantv2/src/ntv2devicefeatures.cpp
@@ -1093,9 +1093,9 @@ bool NTV2DeviceCanDoRS422N (const NTV2DeviceID inDeviceID, const NTV2Channel inC
 
 NTV2AudioSystem NTV2DeviceGetAudioMixerSystem(const NTV2DeviceID inDeviceID)
 {
-	if(inDeviceID != DEVICE_ID_IO4KPLUS || inDeviceID != DEVICE_ID_IO4K)
-		return NTV2_AUDIOSYSTEM_INVALID;
-	return (NTV2AudioSystem) (NTV2DeviceGetNumAudioSystems(inDeviceID) + 1);
+	if (inDeviceID == DEVICE_ID_IO4KPLUS  ||  inDeviceID == DEVICE_ID_IO4K)
+		return NTV2AudioSystem(NTV2DeviceGetNumAudioSystems(inDeviceID) + 1);
+	return NTV2_AUDIOSYSTEM_INVALID;
 }
 
 #if !defined (NTV2_DEPRECATE)
