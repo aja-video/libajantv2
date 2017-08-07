6876ccd04d03e2665a78a5006d3cc84a529cef32
Scott <scotth@aja.com>
2017-08-07 13:51:55 -0700

    Add swizzle vregs

--- ajalibraries/ajantv2/includes/ntv2virtualregisters.h
+++ ajalibraries/ajantv2/includes/ntv2virtualregisters.h
@@ -461,8 +461,9 @@ typedef enum
     kVRegAudioMixerSourceMainSelect         = VIRTUALREG_START+515,
     kVRegAudioMixerSourceAux1Select         = VIRTUALREG_START+516,
     kVRegAudioMixerSourceAux2Select         = VIRTUALREG_START+517,
+	kVRegSizzle4kInput						= VIRTUALREG_START+519,
 
-    kVRegLastAJA							= VIRTUALREG_START+518,		///< @brief	The last AJA virtual register slot
+	kVRegLastAJA							= VIRTUALREG_START+520,		///< @brief	The last AJA virtual register slot
 	kVRegFirstOEM							= kVRegLastAJA + 1,			///< @brief	The first virtual register slot available for general use
 	kVRegLast								= VIRTUALREG_START + MAX_NUM_VIRTUAL_REGISTERS - 1	///< @brief	Last virtual register slot
 
