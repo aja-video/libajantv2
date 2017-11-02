11028737c7924bb5f91afd6b1313abc8827f712c
stans <stans@aja.com>
2017-11-01 17:03:22 -0700

    Fix genlockv2 lock for hdmi in select

--- ajalibraries/ajantv2/utilityfiles/retail_services/ntv2deviceservices.cpp
+++ ajalibraries/ajantv2/utilityfiles/retail_services/ntv2deviceservices.cpp
@@ -2601,8 +2601,11 @@ void DeviceServices::SetDeviceXPointPlayback( GeneralFrameFormat format )
 	}
 
 	// The reference (genlock) source: if it's a video input, make sure it matches our current selection
-	ReferenceSelect tempSelect = NTV2DeviceHasGenlockv2(deviceID) ? kVideoIn : mDisplayReferenceSelect;
-	ReferenceSelect refSelect = bDSKNeedsInputRef ? mCaptureReferenceSelect : tempSelect;
+	bool lockV2 = NTV2DeviceHasGenlockv2(deviceID);
+	ReferenceSelect refSelect = bDSKNeedsInputRef ? mDisplayReferenceSelect : mDisplayReferenceSelect;
+	if (lockV2 == true && refSelect == kFreeRun)
+		refSelect = kVideoIn;
+	
 	switch (refSelect)
 	{
 	default:
@@ -2653,7 +2656,10 @@ void DeviceServices::SetDeviceXPointPlayback( GeneralFrameFormat format )
 				}
 				break;
 			case NTV2_Input5Select://Only used by io4k quad id
-				mCard->SetReference(NTV2_REFERENCE_HDMI_INPUT);
+				if (lockV2)
+					mCard->SetReference(NTV2_REFERENCE_FREERUN);
+				else
+					mCard->SetReference(NTV2_REFERENCE_HDMI_INPUT);
 				break;
 			}
 		}
