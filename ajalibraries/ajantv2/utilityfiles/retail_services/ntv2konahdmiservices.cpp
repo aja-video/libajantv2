f63bdf73a9b8e11b27ae0380a5c8adc904b1b371
stans <stans@aja.com>
2018-04-04 22:16:31 -0700

    Bug 15390: KONA HDMI: Follow input is missing

--- ajalibraries/ajantv2/utilityfiles/retail_services/ntv2konahdmiservices.cpp
+++ ajalibraries/ajantv2/utilityfiles/retail_services/ntv2konahdmiservices.cpp
@@ -30,6 +30,47 @@ void KonaHDMIServices::UpdateAutoState (void)
 	}
 }
 
+
+//-------------------------------------------------------------------------------------------------------
+//	GetSelectedInputVideoFormat
+//	Note:	Determine input video format based on input select and fbVideoFormat
+//			which currently is videoformat of ch1-framebuffer
+//-------------------------------------------------------------------------------------------------------
+NTV2VideoFormat KonaHDMIServices::GetSelectedInputVideoFormat(
+											NTV2VideoFormat fbVideoFormat,
+											NTV2SDIInputFormatSelect* inputFormatSelect)
+{
+	(void)fbVideoFormat;
+	NTV2VideoFormat inputFormat = NTV2_FORMAT_UNKNOWN;
+	if (inputFormatSelect)
+		*inputFormatSelect = NTV2_YUVSelect;
+	
+	// Figure out what our input format is based on what is selected
+    switch (mVirtualInputSelect)
+    {
+		case NTV2_Input1Select:
+		case NTV2_Input2Select:
+		case NTV2_Input3Select:
+		case NTV2_Input4Select:
+			{
+				inputFormat = mCard->GetHDMIInputVideoFormat((NTV2Channel)mVirtualInputSelect);
+			
+				NTV2LHIHDMIColorSpace hdmiInputColor;
+				mCard->GetHDMIInputColor(hdmiInputColor, (NTV2Channel)mVirtualInputSelect);
+
+				if (inputFormatSelect)
+					*inputFormatSelect = (hdmiInputColor == NTV2_LHIHDMIColorSpaceYCbCr) ? NTV2_YUVSelect : NTV2_RGBSelect;
+			}
+			break;
+
+		default:
+			break;
+	}
+	
+	return inputFormat;
+}
+
+
 //-------------------------------------------------------------------------------------------------------
 //	SetDeviceXPointPlayback
 //-------------------------------------------------------------------------------------------------------
