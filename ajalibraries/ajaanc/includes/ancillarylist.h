0cd711996f3f9a4632dcccc5669eb4075535485e
MrBill <bills@aja.com>
2018-04-11 18:24:46 -0700

    Docs updates (RTP info & IoIP placeholder image)

--- ajalibraries/ajaanc/includes/ancillarylist.h
+++ ajalibraries/ajaanc/includes/ancillarylist.h
@@ -32,19 +32,20 @@ typedef std::map <uint16_t, AJAAncillaryDataType>	AJAAncillaryAnalogTypeMap;
 
 				<b>Transmit:</b> Put My Packets Into Other Buffer Destinations
 				-	Call AJAAncillaryList::AddAncillaryData for each packet to be added to me.
-				-	For AJA hardware, it's best to call AJAAncillaryList::SortListByLocation before sending it to another buffer.
-				-	For encoding into a "tall" or "taller" VANC frame buffer, call AJAAncillaryList::WriteVANCData.
+				-	For AJA hardware, it's best to call AJAAncillaryList::SortListByLocation before transmission.
+				-	For encoding into a "tall" or "taller" VANC frame buffer, call AJAAncillaryList::GetVANCTransmitData.
 				-	To fill the Anc buffer used in the AUTOCIRCULATE_TRANSFER::SetAncBuffers function,
-					-	call AJAAncillaryList::GetAncillaryDataTransmitData for SDI (see \ref ancgumpformat).
-					-	call AJAAncillaryList::WriteIPAncData for IP/RTP.
+					-	call AJAAncillaryList::GetSDITransmitData for SDI (see \ref ancgumpformat).
+					-	call AJAAncillaryList::GetIPTransmitData for IP/RTP (see \ref ancrtpformat).
 
 				<b>Receive:</b> Create Packet List(s) From Other Buffer Sources
-				-	For a "tall" or "taller" VANC frame geometry, call the AJAAncillaryList::SetFromVANCData class method,
-					passing it an NTV2_POINTER that references the frame buffer, and an NTV2FormatDescriptor that describes the raster.
-					It will return two packet lists.
+				-	For decoding VANC lines in a "tall" or "taller" frame buffer,
+					call the AJAAncillaryList::SetFromVANCData class method,
+					passing it an NTV2_POINTER that references the frame buffer (or just the VANC portion of one),
+					and an NTV2FormatDescriptor that describes the raster.
 				-	For newer devices with Anc extractors, using the buffer(s) used in the AUTOCIRCULATE_TRANSFER::SetAncBuffers call...
-					-	call AJAAncillaryList::SetFromSDIAncData for SDI (use \ref ancgumpformat);
-					-	call AJAAncillaryList::SetFromIPAncData for IP/RTP.
+					-	call AJAAncillaryList::SetFromSDIAncData for SDI (see \ref ancgumpformat);
+					-	call AJAAncillaryList::SetFromIPAncData for IP/RTP (see \ref ancrtpformat).
 
 	@warning	I am not thread-safe! When any of my non-const methods are called by one thread, do not call any of my
 				methods from any other thread.
