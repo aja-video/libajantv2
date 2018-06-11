6c1c23b6dfeee956cd355059735a856f99dfdcf6
Phil Montoya <philm@aja.com>
2018-06-11 10:02:27 -0700

    Issue reconfig for TX on 2022 when video format changes

--- ajalibraries/ajantv2/utilityfiles/retail_services/ntv2ioip2022services.h
+++ ajalibraries/ajantv2/utilityfiles/retail_services/ntv2ioip2022services.h
@@ -32,6 +32,7 @@ protected:
     CNTV2Config2022     * config;
 
     NTV2Mode				mFb1ModeLast;
+    NTV2VideoFormat			mFb1VideoFormatLast;
 };
 
 
--- ajalibraries/ajantv2/utilityfiles/retail_services/ntv2konaip22services.cpp
+++ ajalibraries/ajantv2/utilityfiles/retail_services/ntv2konaip22services.cpp
@@ -25,6 +25,7 @@ KonaIP22Services::KonaIP22Services()
 {
     config = NULL;
     mFb1ModeLast = NTV2_MODE_INVALID;
+    mFb1VideoFormatLast = NTV2_FORMAT_UNKNOWN;
 }
 
  KonaIP22Services::~KonaIP22Services()
@@ -2668,7 +2669,8 @@ void KonaIP22Services::SetDeviceMiscRegisters()
                         if (NotEqual(txHwConfig, mTx2022Config3, m2022_7Mode) ||
                             configErr ||
                             enable2022_7Card != m2022_7Mode ||
-                            mFb1ModeLast != mFb1Mode)
+                            mFb1ModeLast != mFb1Mode ||
+                            mFb1VideoFormatLast != mFb1VideoFormat)
                         {
                             config->SetTxChannelEnable(NTV2_CHANNEL3, false);
                             SetTxConfig(config, NTV2_CHANNEL3, m2022_7Mode);
@@ -2716,7 +2718,8 @@ void KonaIP22Services::SetDeviceMiscRegisters()
                         if (NotEqual(txHwConfig2, mTx2022Config4, m2022_7Mode) ||
                             configErr ||
                             enable2022_7Card != m2022_7Mode ||
-                            mFb1ModeLast != mFb1Mode)
+                            mFb1ModeLast != mFb1Mode ||
+                            mFb1VideoFormatLast != mFb1VideoFormat)
                         {
                             config->SetTxChannelEnable(NTV2_CHANNEL4, false);
                             SetTxConfig(config, NTV2_CHANNEL4, m2022_7Mode);
@@ -2734,6 +2737,8 @@ void KonaIP22Services::SetDeviceMiscRegisters()
                 SetIPError(NTV2_CHANNEL4,kErrTxConfig,NTV2IpErrInvalidConfig);
 
             mFb1ModeLast = mFb1Mode;
+            mFb1VideoFormatLast = mFb1VideoFormatLast;
+
             config->SetIPServicesControl(true, false);
         }
     }
