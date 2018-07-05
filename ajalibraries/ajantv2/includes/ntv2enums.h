00e4aab9f99ced0409a555c21ebfa8b4de96161b
stans <stans@aja.com>
2018-07-05 16:01:12 -0700

    Add auto mode to NTV2OutputVideoSelect

--- ajalibraries/ajantv2/includes/ntv2enums.h
+++ ajalibraries/ajantv2/includes/ntv2enums.h
@@ -1923,6 +1923,7 @@ typedef enum
     NTV2_Quadrant4Select,
     NTV2_Quarter4k,
     NTV2_4kHalfFrameRate,
+	NTV2_AutoOutputSelect,
     NTV2_MAX_NUM_OutputVideoSelectEnums
 } NTV2OutputVideoSelect;
 
