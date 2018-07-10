57a661a3ed1ed80074c020c412626b14f3cbeb81
Scott Heenan <scotth@aja.com>
2018-07-10 15:26:11 -0700

    Fix build errors

--- ajalibraries/ajantv2/utilityfiles/retail_services/ntv2kona5services.cpp
+++ ajalibraries/ajantv2/utilityfiles/retail_services/ntv2kona5services.cpp
@@ -71,9 +71,9 @@ NTV2VideoFormat Kona5Services::GetSelectedInputVideoFormat(
                 *inputColorSpace = mSDIInput1ColorSpace;
             break;
 
-        case NTV2_DualLinkInputSelect:
-        case NTV2_DualLink4xSdi4k:
-        case NTV2_DualLink2xSdi4k:
+        case NTV2_Input2xDLHDSelect:
+        case NTV2_Input4x4kSelect:
+        case NTV2_Input2x4kSelect:
             inputFormat = GetSdiInVideoFormat(0, fbVideoFormat);
             if (inputColorSpace)
                 *inputColorSpace = mSDIInput1ColorSpace;
@@ -190,7 +190,7 @@ void Kona5Services::SetDeviceXPointPlayback ()
         inputXptYuv2 = NTV2_XptSDIIn2DS2;
     }
     // dual link select
-    else if (mVirtualInputSelect == NTV2_DualLinkInputSelect)
+    else if (mVirtualInputSelect == NTV2_Input2xDLHDSelect)
     {
         inputXptYuv1 = NTV2_XptSDIIn1;
         inputXptYuv2 = NTV2_XptSDIIn2;
@@ -1259,7 +1259,7 @@ void Kona5Services::SetDeviceXPointPlayback ()
                     mCard->Connect (NTV2_XptMixer1BGVidInput, NTV2_XptHDMIIn);
                     mCard->Connect (NTV2_XptMixer1BGKeyInput, NTV2_XptHDMIIn);
                 }
-                else if (mVirtualInputSelect == NTV2_DualLinkInputSelect)
+                else if (mVirtualInputSelect == NTV2_Input2xDLHDSelect)
                 {
                     mCard->Connect (NTV2_XptMixer1BGVidInput, NTV2_XptDuallinkIn1);
                     mCard->Connect (NTV2_XptMixer1BGKeyInput, NTV2_XptDuallinkIn1);
@@ -1317,7 +1317,7 @@ void Kona5Services::SetDeviceXPointPlayback ()
                     mCard->Connect (NTV2_XptMixer1BGVidInput, NTV2_XptHDMIIn);
                     mCard->Connect (NTV2_XptMixer1BGKeyInput, NTV2_XptHDMIIn);
                 }
-                else if (mVirtualInputSelect == NTV2_DualLinkInputSelect)
+                else if (mVirtualInputSelect == NTV2_Input2xDLHDSelect)
                 {
                     mCard->Connect (NTV2_XptMixer1BGVidInput, NTV2_XptDuallinkIn1);
                     mCard->Connect (NTV2_XptMixer1BGKeyInput, NTV2_XptDuallinkIn1);
@@ -1476,8 +1476,8 @@ void Kona5Services::SetDeviceXPointCapture ()
     //												  (m4kTransportOutSelection == NTV2_4kTransport_12g_6g_1wire ||
     //												   m4kTransportOutSelection == NTV2_4kTransport_PixelInterleave);
     bool						b2FbLevelBHfr		= IsVideoFormatB(mFb1VideoFormat);
-    bool						b2xQuadIn			= b4K && !b4kHfr && (mVirtualInputSelect == NTV2_DualLink2xSdi4k);
-    bool						b4xQuadIn			= b4K && (mVirtualInputSelect == NTV2_DualLink4xSdi4k);
+    bool						b2xQuadIn			= b4K && !b4kHfr && (mVirtualInputSelect == NTV2_Input2x4kSelect);
+    bool						b4xQuadIn			= b4K && (mVirtualInputSelect == NTV2_Input4x4kSelect);
     bool						b2xQuadOut			= b4K && (m4kTransportOutSelection == NTV2_4kTransport_Quadrants_2wire);
     //bool						b4xQuadOut			= b4K && (m4kTransportOutSelection == NTV2_4kTransport_Quadrants_4wire);
     int							bFb1Disable			= 0;		// Assume Channel 1 is NOT disabled by default
@@ -1494,7 +1494,7 @@ void Kona5Services::SetDeviceXPointCapture ()
     // swap quad mode
     ULWord						selectSwapQuad		= 0;
     mCard->ReadRegister(kVRegSwizzle4kInput, selectSwapQuad);
-    bool						bQuadSwap			= b4K == true && mVirtualInputSelect == NTV2_DualLink4xSdi4k && selectSwapQuad != 0;
+    bool						bQuadSwap			= b4K == true && mVirtualInputSelect == NTV2_Input4x4kSelect && selectSwapQuad != 0;
 
     // SMPTE 425 (2pi)
     bool						bVpid2x2piIn		= false;
@@ -1552,7 +1552,7 @@ void Kona5Services::SetDeviceXPointCapture ()
         inHdRGB1 = NTV2_XptDuallinkIn1;
     }
     // dual link select
-    else if (mVirtualInputSelect == NTV2_DualLinkInputSelect)
+    else if (mVirtualInputSelect == NTV2_Input2xDLHDSelect)
     {
         inHdYUV1 = NTV2_XptSDIIn1;
         inHdYUV2 = NTV2_XptSDIIn2;
@@ -2901,7 +2901,7 @@ void Kona5Services::SetDeviceMiscRegisters ()
 
     bool b2xQuadOut = (    ((mFb1Mode != NTV2_MODE_CAPTURE) && (b4K && !b4kHfr && m4kTransportOutSelection == NTV2_4kTransport_Quadrants_2wire))
                          || ((mFb1Mode == NTV2_MODE_CAPTURE) && b4K && !b4kHfr && m4kTransportOutSelection == NTV2_4kTransport_Quadrants_2wire));
-    bool b2xQuadIn =  (mFb1Mode == NTV2_MODE_CAPTURE) && (b4K && !b4kHfr && mVirtualInputSelect  == NTV2_DualLink2xSdi4k);
+    bool b2xQuadIn =  (mFb1Mode == NTV2_MODE_CAPTURE) && (b4K && !b4kHfr && mVirtualInputSelect  == NTV2_Input2x4kSelect);
 
     // all 3Gb transport out
     // b3GbOut = (b1x3GbOut + !2wire) | (4k + rgb) | (4khfr + 3gb)
