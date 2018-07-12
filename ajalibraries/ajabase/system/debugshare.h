d7479bca0fb628f1113ba7f611a76548193f1fd8
Scott Heenan <scotth@aja.com>
2018-07-12 16:31:21 -0700

    Added avid debug id Fixed IoIP lock->unlock->lock issue Bug:15191 AJA-107...

--- ajalibraries/ajabase/system/debugshare.h
+++ ajalibraries/ajabase/system/debugshare.h
@@ -75,6 +75,7 @@ typedef enum _AJADebugUnit
     AJA_DebugUnit_BFT						= 34,
     AJA_DebugUnit_PnP						= 35,
     AJA_DebugUnit_Persistence               = 36,
+    AJA_DebugUnit_Avid                      = 37,
 
     // to add a new unit:
     //
@@ -93,9 +94,8 @@ typedef enum _AJADebugUnit
     // if no more unused units
     //   * set AJA_DebugUnit_FirstUnused to the same value as AJA_DebugUnit_Size
     //
-    AJA_DebugUnit_FirstUnused               = 37,
-    AJA_DebugUnit_Unused_37                 = AJA_DebugUnit_FirstUnused,
-    AJA_DebugUnit_Unused_38                 = 38,
+    AJA_DebugUnit_FirstUnused               = 38,
+    AJA_DebugUnit_Unused_38                 = AJA_DebugUnit_FirstUnused,
     AJA_DebugUnit_Unused_39                 = 39,
     AJA_DebugUnit_Unused_40                 = 40,
     AJA_DebugUnit_Unused_41                 = 41,
