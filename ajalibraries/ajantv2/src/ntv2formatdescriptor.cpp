b778a1a3fe071d8ea197a75f3d4d1923818a44b1
MrBill <bills@aja.com>
2018-07-20 12:47:20 -0700

    Fixed build

--- ajalibraries/ajantv2/src/ntv2formatdescriptor.cpp
+++ ajalibraries/ajantv2/src/ntv2formatdescriptor.cpp
@@ -1105,11 +1105,23 @@ ostream & NTV2FormatDescriptor::PrintSMPTELineNumber (ostream & inOutStream, con
 }
 
 
-//	Q:	WHY IS NTV2SmpteLineNumber's CONSTRUCTOR HERE?
+//	Q:	WHY IS NTV2SmpteLineNumber's CONSTRUCTOR & GetLastLine IMPLEMENTATION HERE?
 //	A:	TO USE THE SAME LineNumbersF1/LineNumbersF2 TABLES (above)
 
 NTV2SmpteLineNumber::NTV2SmpteLineNumber (const NTV2Standard inStandard)
 {
 	NTV2_ASSERT (inStandard < sizeof(LineNumbersF1) / sizeof(ULWord));
 	*this = NTV2SmpteLineNumber (LineNumbersF1[inStandard],  LineNumbersF2[inStandard],  inStandard != NTV2_STANDARD_525,  inStandard);
+}
+
+
+ULWord NTV2SmpteLineNumber::GetLastLine (const NTV2FieldID inRasterFieldID) const
+{
+	if (!NTV2_IS_VALID_FIELD(inRasterFieldID))
+		return 0;
+
+	if (inRasterFieldID == NTV2_FIELD0)
+		return firstFieldTop ? LineNumbersF1Last[mStandard] : LineNumbersF2Last[mStandard];
+	else
+		return firstFieldTop ? LineNumbersF2Last[mStandard] : LineNumbersF1Last[mStandard];
 }
