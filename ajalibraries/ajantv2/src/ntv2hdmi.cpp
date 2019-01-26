8d75e6c5b08c067dd8f22e6966008e616898a4e0
davids <davids@aja.com>
2019-01-25 16:29:37 -0800

    fix new HDMI 12 bit setting

--- ajalibraries/ajantv2/src/ntv2hdmi.cpp
+++ ajalibraries/ajantv2/src/ntv2hdmi.cpp
@@ -370,17 +370,17 @@ bool CNTV2Card::SetHDMIOutBitDepth (const NTV2HDMIBitDepth value)
 	if (value == NTV2_HDMI12Bit)
 	{
 		ret &= WriteRegister (kRegHDMIOutControl, 1, kLHIRegMaskHDMIOutBitDepth, kLHIRegShiftHDMIOutBitDepth);
-		ret &= WriteRegister (kRegHDMIOutControl, 1, kRegMaskHDMIOut12Bit, kRegShiftHDMIOut12Bit);
+		ret &= WriteRegister (kRegHDMIInputControl, 1, kRegMaskHDMIOut12Bit, kRegShiftHDMIOut12Bit);
 	}
 	else if (value == NTV2_HDMI10Bit)
 	{
 		ret &= WriteRegister (kRegHDMIOutControl, 1, kLHIRegMaskHDMIOutBitDepth, kLHIRegShiftHDMIOutBitDepth);
-		ret &= WriteRegister (kRegHDMIOutControl, 0, kRegMaskHDMIOut12Bit, kRegShiftHDMIOut12Bit);
+		ret &= WriteRegister (kRegHDMIInputControl, 0, kRegMaskHDMIOut12Bit, kRegShiftHDMIOut12Bit);
 	}
 	else
 	{
 		ret &= WriteRegister (kRegHDMIOutControl, 0, kLHIRegMaskHDMIOutBitDepth, kLHIRegShiftHDMIOutBitDepth);
-		ret &= WriteRegister (kRegHDMIOutControl, 0, kRegMaskHDMIOut12Bit, kRegShiftHDMIOut12Bit);
+		ret &= WriteRegister (kRegHDMIInputControl, 0, kRegMaskHDMIOut12Bit, kRegShiftHDMIOut12Bit);
 	}
 
 	return ret;
@@ -396,7 +396,7 @@ bool CNTV2Card::GetHDMIOutBitDepth (NTV2HDMIBitDepth & outValue)
 		return false;
 
 	ret &= CNTV2DriverInterface::ReadRegister (kRegHDMIOutControl, d10, kLHIRegMaskHDMIOutBitDepth, kLHIRegShiftHDMIOutBitDepth);
-	ret &= CNTV2DriverInterface::ReadRegister (kRegHDMIOutControl, d12,  kRegMaskHDMIOut12Bit, kRegShiftHDMIOut12Bit);
+	ret &= CNTV2DriverInterface::ReadRegister (kRegHDMIInputControl, d12,  kRegMaskHDMIOut12Bit, kRegShiftHDMIOut12Bit);
 
 	if (!ret)
 		return false;
