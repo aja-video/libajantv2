9090f501cda30957c12c331f73309db1708d34b7
MrBill <bills@aja.com>
2019-09-11 11:16:57 -0700

    Fixed linux driver build

--- ajalibraries/ajantv2/src/ntv2devicefeatures.cpp
+++ ajalibraries/ajantv2/src/ntv2devicefeatures.cpp
@@ -1084,8 +1084,8 @@ bool NTV2DeviceCanDoRS422N (const NTV2DeviceID inDeviceID, const NTV2Channel inC
 
 NTV2AudioSystem NTV2DeviceGetAudioMixerSystem(const NTV2DeviceID inDeviceID)
 {
-	if (::NTV2DeviceGetNumAudioSystems(inDeviceID))
-	    return NTV2AudioSystem(::NTV2DeviceGetNumAudioSystems(inDeviceID) + 1);
+	if (NTV2DeviceGetNumAudioSystems(inDeviceID))
+	    return (NTV2AudioSystem)(NTV2DeviceGetNumAudioSystems(inDeviceID) + 1);
 	return NTV2_AUDIOSYSTEM_INVALID;
 }
 
