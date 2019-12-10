ca0ee76a9097bf0af77f0b568373cffb61683a53
MrBill <bills@aja.com>
2019-12-10 12:44:54 -0800

    Fixed AutoCirculateInitForInput/Output Functions FrameCount param only used when start/end frame numbers are both zero; Updated docs to clarify

--- ntv2ccplayer/ntv2ccplayer.cpp
+++ ntv2ccplayer/ntv2ccplayer.cpp
@@ -1563,12 +1563,13 @@ void NTV2CCPlayer::PlayoutFrames (void)
 	mDevice.AutoCirculateStop (mConfig.fOutputChannel);	//	Maybe some other app left this A/C channel running
 	if (NTV2_IS_SD_VIDEO_FORMAT(mConfig.fVideoFormat)  &&  mConfig.fSuppressLine21  &&  mConfig.fSuppress608)
 		cerr << "## WARNING:  SD video with '--noline21' option and '--no608' option won't produce captions" << endl;
-	mDevice.AutoCirculateInitForOutput (mConfig.fOutputChannel,
-										mConfig.fFrames.count(),	//	numFrames (zero if specifying range)
-										audioSystem,
-										acOptionFlags,
-										1,							//	numChannels to gang
-										mConfig.fFrames.firstFrame(), mConfig.fFrames.lastFrame());
+	if (!mDevice.AutoCirculateInitForOutput (mConfig.fOutputChannel,
+											mConfig.fFrames.count(),	//	numFrames (zero if specifying range)
+											audioSystem,
+											acOptionFlags,
+											1,							//	numChannels to gang
+											mConfig.fFrames.firstFrame(), mConfig.fFrames.lastFrame()))
+		{cerr << "## ERROR: AutoCirculateInitForOutput failed" << endl;  mPlayerQuit = true;}
 	mDevice.AutoCirculateStart (mConfig.fOutputChannel);
 
 	//	Repeat until time to quit...
