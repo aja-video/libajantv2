4e05ac3f6e003e7e3d626fa65771a9c34a30fd0b
Phil Montoya <philm@aja.com>
2020-04-06 11:34:03 -0700

    fix windows compile

--- ajalibraries/ajantv2/src/ntv2register.cpp
+++ ajalibraries/ajantv2/src/ntv2register.cpp
@@ -207,8 +207,8 @@ static const ULWord	gChannelToSDIInputRateHighShift []		= {	kRegShiftInput1Frame
 
 static const ULWord	gChannelToSDIInputGeometryMask []		= {	kRegMaskInput1Geometry,				kRegMaskInput2Geometry,				kRegMaskInput1Geometry,				kRegMaskInput2Geometry,
 																kRegMaskInput1Geometry,				kRegMaskInput2Geometry,				kRegMaskInput1Geometry,				kRegMaskInput2Geometry,				0};
-static const ULWord	gChannelToSDIInputGeometryHighMask []	= {	kRegMaskInput1GeometryHigh,			kRegMaskInput2GeometryHigh,			kRegMaskInput1GeometryHigh,			kRegMaskInput2GeometryHigh,
-																kRegMaskInput1GeometryHigh,			kRegMaskInput2GeometryHigh,			kRegMaskInput1GeometryHigh,			kRegMaskInput2GeometryHigh,			0};
+static const ULWord	gChannelToSDIInputGeometryHighMask []	= {	kRegMaskInput1GeometryHigh,			(ULWord)kRegMaskInput2GeometryHigh,	kRegMaskInput1GeometryHigh,			(ULWord)kRegMaskInput2GeometryHigh,
+                                                                kRegMaskInput1GeometryHigh,			(ULWord)kRegMaskInput2GeometryHigh,	kRegMaskInput1GeometryHigh,			(ULWord)kRegMaskInput2GeometryHigh,			0};
 static const ULWord	gChannelToSDIInputGeometryShift []		= {	kRegShiftInput1Geometry,			kRegShiftInput2Geometry,			kRegShiftInput1Geometry,			kRegShiftInput2Geometry,
 																kRegShiftInput1Geometry,			kRegShiftInput2Geometry,			kRegShiftInput1Geometry,			kRegShiftInput2Geometry,			0};
 static const ULWord	gChannelToSDIInputGeometryHighShift []	= {	kRegShiftInput1GeometryHigh,		kRegShiftInput2GeometryHigh,		kRegShiftInput1GeometryHigh,		kRegShiftInput2GeometryHigh,
