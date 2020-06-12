393d38b9935f4b72933b6547dfaabdd222f26def
MrBill <bills@aja.com>
2020-06-12 09:22:11 -0700

    Docs tweaks Finally documented the NTV2FieldID enums

--- ajalibraries/ajantv2/includes/ntv2card.h
+++ ajalibraries/ajantv2/includes/ntv2card.h
@@ -3344,7 +3344,7 @@ public:
 					CNTV2Card::SetOutputFrame calls (and others) actually take effect.
 		@param[in]	inValue			Specifies the ::NTV2RegisterWriteMode to set for the FrameStore.
 		@param[in]	inFrameStore	Specifies the FrameStore of interest as an ::NTV2Channel, a zero-based index number.
-									If omitted, defaults to NTV2_CHANNEL1.
+									If omitted, defaults to ::NTV2_CHANNEL1.
 		@see		CNTV2Card::GetRegisterWriteMode, CNTV2Card::SetInputFrame, CNTV2Card::SetOutputFrame, \ref fieldframeinterrupts
 	**/
 	AJA_VIRTUAL bool	SetRegisterWriteMode (const NTV2RegisterWriteMode inValue, const NTV2Channel inFrameStore = NTV2_CHANNEL1);
@@ -3354,7 +3354,7 @@ public:
 					CNTV2Card::SetInputFrame or CNTV2Card::SetOutputFrame calls (and others) actually take effect.
 		@param[out]	outValue		Receives the ::NTV2RegisterWriteMode that's currently being used by the FrameStore.
 		@param[in]	inFrameStore	Specifies the FrameStore of interest as an ::NTV2Channel, a zero-based index number.
-									If omitted, defaults to NTV2_CHANNEL1.
+									If omitted, defaults to ::NTV2_CHANNEL1.
 		@see		CNTV2Card::SetRegisterWriteMode, CNTV2Card::SetInputFrame, CNTV2Card::SetOutputFrame, \ref fieldframeinterrupts
 	**/
 	AJA_VIRTUAL bool	GetRegisterWriteMode (NTV2RegisterWriteMode & outValue, const NTV2Channel inFrameStore = NTV2_CHANNEL1);
