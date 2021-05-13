452e83c9ab9d2bb26c42100907ab06180495b950
stans <stans@aja.com>
2021-05-13 12:57:59 -0600

    Add hdmi sub sample option
    part I

--- ajalibraries/ajantv2/includes/ntv2enums.h
+++ ajalibraries/ajantv2/includes/ntv2enums.h
@@ -2453,7 +2453,7 @@ typedef enum
 	NTV2_Quadrant3Select,
 	NTV2_Quadrant4Select,
 	NTV2_Quarter4k,
-	NTV2_4kHalfFrameRate,
+	NTV2_4kHalfFrameRate,					// deprecated
 	NTV2_QuadView,
 	NTV2_AutoOutputSelect,
 	NTV2_MAX_NUM_OutputVideoSelectEnums
