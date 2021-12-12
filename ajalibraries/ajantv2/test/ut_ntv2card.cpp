dc1e81b1b7641e505c435572ccacba031aa0772f
Paul Hindt <paulh@aja.com>
2021-12-11 23:06:19 -0800

    ut_ntv2card: Hook up global args reading

--- ajalibraries/ajantv2/test/ut_ntv2card.cpp
+++ ajalibraries/ajantv2/test/ut_ntv2card.cpp
@@ -11,6 +11,18 @@
 #include "ajantv2/includes/ntv2card.h"
 #include "ajantv2/includes/ntv2devicefeatures.h"
 
+static int
+argparse_help(struct argparse *self, const struct argparse_option *option)
+{
+    (void)option;
+    argparse_usage(self);
+    return 0;
+}
+
+#define OPT_ARGPARSE_HELP()       OPT_BOOLEAN('h', "help", NULL,        \
+                                     "show this help message and exit", \
+                                     argparse_help, 0, OPT_NONEG)
+
 static constexpr size_t kAudioSize1MiB = 0xff000;
 static constexpr size_t kAudioSize2MiB = kAudioSize1MiB * 2;
 static constexpr size_t kAudioSize4MiB = kAudioSize1MiB * 4;
@@ -22,15 +34,20 @@ static constexpr uint8_t kSDILegalMax8Bit = 0xfb;
 static constexpr uint16_t kSDILegalMax10Bit = 0x3fb;
 static constexpr uint32_t kSDILegalMax12Bit = 0xffc;
 
-static CNTV2Card* gCardInstance = NULL;
+struct TestOptions {
+    uint32_t card_index;
+};
+
+static TestOptions* gOpts = NULL;
+static CNTV2Card* gCard = NULL;
 
 void ntv2card_general_marker() {}
 TEST_SUITE("CNTV2Card General" * doctest::description("CNTV2Card general tests")) {
     TEST_CASE("IsOpen") {
-        CHECK(gCardInstance->IsOpen() == true);
+        CHECK(gCard->IsOpen() == true);
     }
     TEST_CASE("GetDeviceID") {
-        CHECK(gCardInstance->GetDeviceID() != DEVICE_ID_NOTFOUND);
+        CHECK(gCard->GetDeviceID() != DEVICE_ID_NOTFOUND);
     }
 }
 
@@ -43,9 +60,9 @@ TEST_SUITE("CNTV2Card DMA" * doctest::description("CNTV2Card DMA tests")) {
         uint8_t* readback = (uint8_t*)malloc(kAudioSize4MiB);
         qa::RandomPattern<uint32_t>(0, 0, UINT32_MAX, buffer, kAudioSize4MiB, ByteOrder::LittleEndian);
         qa::RandomPattern<uint32_t>(0, 0, UINT32_MAX, readback, kAudioSize4MiB, ByteOrder::LittleEndian);
-        CHECK(gCardInstance->SetMode(channel, NTV2_MODE_DISPLAY) == true);
-        CHECK(gCardInstance->DMAWriteAudio(audio_sys, reinterpret_cast<const ULWord*>(buffer), 0, kAudioSize4MiB) == true);
-        CHECK(gCardInstance->DMAReadAudio(audio_sys, reinterpret_cast<ULWord*>(readback), 0, kAudioSize4MiB) == true);
+        CHECK(gCard->SetMode(channel, NTV2_MODE_DISPLAY) == true);
+        CHECK(gCard->DMAWriteAudio(audio_sys, reinterpret_cast<const ULWord*>(buffer), 0, kAudioSize4MiB) == true);
+        CHECK(gCard->DMAReadAudio(audio_sys, reinterpret_cast<ULWord*>(readback), 0, kAudioSize4MiB) == true);
         free((void*)buffer);
         free((void*)readback);
         buffer = NULL;
@@ -58,10 +75,10 @@ TEST_SUITE("CNTV2Card DMA" * doctest::description("CNTV2Card DMA tests")) {
         uint8_t* readback = (uint8_t*)malloc(kFrameSize8MiB);
         qa::RandomPattern<uint32_t>(0, 0, UINT32_MAX, buffer, kFrameSize8MiB, ByteOrder::LittleEndian);
         qa::RandomPattern<uint32_t>(0, 0, UINT32_MAX, readback, kFrameSize8MiB, ByteOrder::LittleEndian);
-        CHECK(gCardInstance->SetMode(channel, NTV2_MODE_DISPLAY) == true);
-        CHECK(gCardInstance->SetOutputFrame(channel, frame_num) == true);
-        CHECK(gCardInstance->DMAWriteFrame(frame_num, reinterpret_cast<const ULWord*>(buffer), (ULWord)kFrameSize8MiB) == true);
-        CHECK(gCardInstance->DMAReadFrame(frame_num, reinterpret_cast<ULWord*>(&readback[0]), (ULWord)kFrameSize8MiB) == true);
+        CHECK(gCard->SetMode(channel, NTV2_MODE_DISPLAY) == true);
+        CHECK(gCard->SetOutputFrame(channel, frame_num) == true);
+        CHECK(gCard->DMAWriteFrame(frame_num, reinterpret_cast<const ULWord*>(buffer), (ULWord)kFrameSize8MiB) == true);
+        CHECK(gCard->DMAReadFrame(frame_num, reinterpret_cast<ULWord*>(&readback[0]), (ULWord)kFrameSize8MiB) == true);
         CHECK(memcmp(readback, buffer, kFrameSize8MiB) == 0);
         free((void*)buffer);
         free((void*)readback);
@@ -70,20 +87,20 @@ TEST_SUITE("CNTV2Card DMA" * doctest::description("CNTV2Card DMA tests")) {
     }
     TEST_CASE("DMAWriteFrame/DMAReadFrame All Frames" * doctest::description("DMAWriteFrame/DMAReadFrame all frames random data test")) {
         NTV2Channel channel = NTV2_CHANNEL1;
-        const ULWord num_frame_buffers = gCardInstance->GetNumFrameBuffers();
+        const ULWord num_frame_buffers = gCard->GetNumFrameBuffers();
         uint8_t* buffer = (uint8_t*)malloc(kFrameSize8MiB);
         uint8_t* readback = (uint8_t*)malloc(kFrameSize8MiB);
         size_t num_writes = 0;
         size_t num_reads = 0;
         size_t num_cmps = 0;
         for (ULWord i = 0; i < num_frame_buffers; i++) {
-            gCardInstance->SetMode(channel, NTV2_MODE_DISPLAY);
-            gCardInstance->SetOutputFrame(channel, i);
+            gCard->SetMode(channel, NTV2_MODE_DISPLAY);
+            gCard->SetOutputFrame(channel, i);
             qa::RandomPattern<uint32_t>(0, 0, UINT32_MAX, buffer, kFrameSize8MiB, ByteOrder::LittleEndian);
             qa::RandomPattern<uint32_t>(0, 0, UINT32_MAX, readback, kFrameSize8MiB, ByteOrder::LittleEndian);
-            if (gCardInstance->DMAWriteFrame(i, reinterpret_cast<const ULWord*>(buffer), (ULWord)kFrameSize8MiB))
+            if (gCard->DMAWriteFrame(i, reinterpret_cast<const ULWord*>(buffer), (ULWord)kFrameSize8MiB))
                 num_writes++;
-            if (gCardInstance->DMAReadFrame(i, reinterpret_cast<ULWord*>(readback), (ULWord)kFrameSize8MiB))
+            if (gCard->DMAReadFrame(i, reinterpret_cast<ULWord*>(readback), (ULWord)kFrameSize8MiB))
                 num_reads++;
             if (memcmp(readback, buffer, kFrameSize8MiB) == 0)
                 num_cmps++;
@@ -103,17 +120,17 @@ TEST_SUITE("Framestore Formats" * doctest::description("Framestore widget format
     TEST_CASE("Framestore 1080p30 YUV-8") {
         auto vf = NTV2_FORMAT_1080p_3000;
         auto pf = NTV2_FBF_8BIT_YCBCR;
-        CHECK(gCardInstance->SetMode(NTV2_CHANNEL1, NTV2_MODE_DISPLAY) == true);
-        CHECK(gCardInstance->SetVideoFormat(vf, false, false, NTV2_CHANNEL1) == true);
-        CHECK(gCardInstance->SetFrameBufferFormat(NTV2_CHANNEL1, pf) == true);
+        CHECK(gCard->SetMode(NTV2_CHANNEL1, NTV2_MODE_DISPLAY) == true);
+        CHECK(gCard->SetVideoFormat(vf, false, false, NTV2_CHANNEL1) == true);
+        CHECK(gCard->SetFrameBufferFormat(NTV2_CHANNEL1, pf) == true);
         NTV2FormatDesc fd(vf, pf);
         auto raster_size = fd.GetTotalBytes();
         uint8_t* buffer = (uint8_t*)malloc(kFrameSize8MiB);
         uint8_t* readback = (uint8_t*)malloc(kFrameSize8MiB);
         qa::Generate8Bit(fd.GetRasterHeight(), fd.GetBytesPerRow(), buffer, kSDILegalMin, kSDILegalMax8Bit);
         qa::RandomPattern<uint32_t>(0, 0, UINT32_MAX, readback, fd.GetTotalBytes(), ByteOrder::LittleEndian);
-        CHECK(gCardInstance->DMAWriteFrame(0, reinterpret_cast<const ULWord*>(buffer), raster_size) == true);
-        CHECK(gCardInstance->DMAReadFrame(0, reinterpret_cast<ULWord*>(readback), raster_size) == true);
+        CHECK(gCard->DMAWriteFrame(0, reinterpret_cast<const ULWord*>(buffer), raster_size) == true);
+        CHECK(gCard->DMAReadFrame(0, reinterpret_cast<ULWord*>(readback), raster_size) == true);
         CHECK(memcmp(buffer, readback, raster_size) == 0);
         free((void*)buffer);
         free((void*)readback);
@@ -123,17 +140,17 @@ TEST_SUITE("Framestore Formats" * doctest::description("Framestore widget format
     TEST_CASE("Framestore UHDp30 YUV-8") {
         auto vf = NTV2_FORMAT_4x1920x1080p_3000;
         auto pf = NTV2_FBF_8BIT_YCBCR;
-        CHECK(gCardInstance->SetMode(NTV2_CHANNEL1, NTV2_MODE_DISPLAY) == true);
-        CHECK(gCardInstance->SetVideoFormat(vf, false, false, NTV2_CHANNEL1) == true);
-        CHECK(gCardInstance->SetFrameBufferFormat(NTV2_CHANNEL1, pf) == true);
+        CHECK(gCard->SetMode(NTV2_CHANNEL1, NTV2_MODE_DISPLAY) == true);
+        CHECK(gCard->SetVideoFormat(vf, false, false, NTV2_CHANNEL1) == true);
+        CHECK(gCard->SetFrameBufferFormat(NTV2_CHANNEL1, pf) == true);
         NTV2FormatDesc fd(vf, pf);
         auto raster_size = fd.GetTotalBytes();
         uint8_t* buffer = (uint8_t*)malloc(kFrameSize32MiB);
         uint8_t* readback = (uint8_t*)malloc(kFrameSize32MiB);
         qa::Generate8Bit(fd.GetRasterHeight(), fd.GetBytesPerRow(), buffer, kSDILegalMin, kSDILegalMax8Bit);
         qa::RandomPattern<uint32_t>(0, 0, UINT32_MAX, readback, fd.GetTotalBytes(), ByteOrder::LittleEndian);
-        CHECK(gCardInstance->DMAWriteFrame(0, reinterpret_cast<const ULWord*>(buffer), raster_size) == true);
-        CHECK(gCardInstance->DMAReadFrame(0, reinterpret_cast<ULWord*>(readback), raster_size) == true);
+        CHECK(gCard->DMAWriteFrame(0, reinterpret_cast<const ULWord*>(buffer), raster_size) == true);
+        CHECK(gCard->DMAReadFrame(0, reinterpret_cast<ULWord*>(readback), raster_size) == true);
         CHECK(memcmp(buffer, readback, raster_size) == 0);
         free((void*)buffer);
         free((void*)readback);
@@ -143,12 +160,54 @@ TEST_SUITE("Framestore Formats" * doctest::description("Framestore widget format
 }
 
 int main(int argc, const char** argv) {
-    doctest::Context ctx(argc, argv);
-    gCardInstance = new CNTV2Card(0, "");
-    int res = ctx.run();
-    if (gCardInstance) {
-        delete gCardInstance;
-        gCardInstance = NULL;
+    gOpts = new TestOptions();
+    gOpts->card_index = 0;
+
+    // copy argv list
+    char** new_argv = (char**)malloc((argc+1) * sizeof *new_argv);
+    for(int i = 0; i < argc; ++i)
+    {
+        size_t length = strlen(argv[i])+1;
+        new_argv[i] = (char*)malloc(length);
+        memcpy(new_argv[i], argv[i], length);
     }
+    new_argv[argc] = NULL;
+
+    // handle global test args
+    static const char *const usage[] = {
+        "ut_ntv2card [options] [[--] args]",
+        "ut_ntv2card [options]",
+        NULL,
+    };
+    struct argparse_option options[] = {
+        OPT_ARGPARSE_HELP(),
+        OPT_GROUP("ut_ntv2card options"),
+        OPT_INTEGER('\0', "card", &gOpts->card_index, "card_index"),
+        OPT_END(),
+    };
+    struct argparse argparse;
+    argparse_init(&argparse, options, usage, 0);
+    argparse_describe(&argparse, "\nntv2 card unit tests",
+        "\nPerform CNTV2Card tests against physical hardware.");
+    argparse_parse(&argparse, argc, (const char**)argv);
+
+    // initialize doctest and handle args
+    doctest::Context ctx(argc, new_argv);
+    gCard = new CNTV2Card(gOpts->card_index, "");
+    int res = ctx.run();
+    if (gCard) {
+        delete gCard;
+        gCard = NULL;
+    }
+
+    // free memory
+    for(int i = 0; i < argc; ++i)
+    {
+        free(new_argv[i]);
+    }
+    free(new_argv);
+
+    delete gOpts;
+
     return ctx.shouldExit() ? res : EXIT_SUCCESS;
 }
