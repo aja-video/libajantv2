beab806b0788c91c7fcf58afd7e68f775f7c1b36
stans <stans@aja.com>
2021-12-20 11:22:37 -0700

    Name fix: QuadView -> MultiView

--- ajalibraries/ajantv2/includes/ntv2enums.h
+++ ajalibraries/ajantv2/includes/ntv2enums.h
@@ -2518,7 +2518,7 @@ typedef enum
 	NTV2_Quadrant4Select,
 	NTV2_Quarter4k,
 	NTV2_4kHalfFrameRate,					// deprecated
-	NTV2_QuadView,
+	NTV2_MultiView,
 	NTV2_AutoOutputSelect,
 	NTV2_MAX_NUM_OutputVideoSelectEnums
 } NTV2OutputVideoSelect;
