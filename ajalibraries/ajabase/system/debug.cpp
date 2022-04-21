1e2eea371e4cab3a91430fb8582d245b5a619855
MrBill <bills@aja.com>
2022-04-21 12:13:13 -0700

    AJADebugStat::NamedStatKeys bug fix: It never initialized the default stat key names map, so it was possible it could always return an empty list

--- ajalibraries/ajabase/system/debug.cpp
+++ ajalibraries/ajabase/system/debug.cpp
@@ -1481,8 +1481,10 @@ bool AJADebugStat::SetStatKeyName (const int inKey, const string & inName)
 
 vector<int> AJADebugStat::NamedStatKeys (void)
 {
-	AJAAutoLock lock(&gStatKeyToStrLock);
 	vector<int> result;
+	AJAAutoLock lock(&gStatKeyToStrLock);
+	if (!gStatKeyToStrReady)
+		InitStatKeyNames();
 	for (StatKeyNameMapConstIter it(gStatKeyToStr.begin());  it != gStatKeyToStr.end();  ++it)
 		result.push_back(it->first);
 	return result;
