f325c8feec58b91c0b2929df48b5aeb41558fb8e
stans <stans@aja.com>
2022-06-13 14:59:27 -0600

    Add support for AJA and NTV2 pixel format conversion

--- ajalibraries/ajabase/common/pixelformat.cpp
+++ ajalibraries/ajabase/common/pixelformat.cpp
@@ -70,8 +70,15 @@ AJAPixelFormatEntry AJAPixelFormatTable[AJA_PixelFormat_Size] =
 	
 	{AJA_PixelFormat_YCBCR8_420PL3,		  AJA_FOURCC('I','4','2','0'),	   8,	AJAScaleTypeNone,	  false,	"8 Bit YCbCr 420 3-Plane"},
 	{AJA_PixelFormat_YCBCR8_422PL3,		  AJA_FOURCC('Y','4','2','B'),	   8,	AJAScaleTypeNone,	  false,	"8 Bit YCbCr 422 3-Plane"},
-	{AJA_PixelFormat_YCBCR10_420PL3LE,	  AJA_FOURCC('Y','2','0','L'),	   10,	AJAScaleTypeNone,	  false,	"10 Bit YCbCr 420 Unpacked 3-Plane LE"},	// ad hoc 4cc
-	{AJA_PixelFormat_YCBCR10_422PL3LE,	  AJA_FOURCC('Y','2','2','L'),	   10,	AJAScaleTypeNone,	  false,	"10 Bit YCbCr 422 Unpacked 3-Plane LE"},	// ad hoc 4cc
+	{AJA_PixelFormat_YCBCR10_420PL3LE,	  AJA_FOURCC('Y','2','0','L'),	   10,	AJAScaleTypeNone,	  false,	"10 Bit YCbCr 420 Unpacked 3-Plane LE"},	// from FFmpeg
+	{AJA_PixelFormat_YCBCR10_422PL3LE,	  AJA_FOURCC('Y','2','2','L'),	   10,	AJAScaleTypeNone,	  false,	"10 Bit YCbCr 422 Unpacked 3-Plane LE"},	// from FFmpeg
+	
+	{AJA_PixelFormat_YCBCR10_420PL2,	  AJA_FOURCC('2','Y','2','0'),	   10,	AJAScaleTypeNone,	  false,	"10 Bit YCbCr 420 Packed 2-Plane LE"},		// AJA ad hoc 4cc
+	{AJA_PixelFormat_YCBCR10_422PL2,	  AJA_FOURCC('2','Y','2','2'),	   10,	AJAScaleTypeNone,	  false,	"10 Bit YCbCr 422 Packed 2-Plane LE"},		// AJA ad hoc 4cc
+	{AJA_PixelFormat_YCBCR8_420PL2,       AJA_FOURCC('2','I','2','0'),	   8,	AJAScaleTypeNone,	  false,	"8 Bit YCbCr 420 2-Plane"},					// AJA ad hoc 4cc
+	{AJA_PixelFormat_YCBCR8_422PL2,       AJA_FOURCC('2','I','2','2'),	   8,	AJAScaleTypeNone,	  false,	"8 Bit YCbCr 422 2-Plane"},					// AJA ad hoc 4cc
+	{AJA_PixelFormat_YCBCR10_420PL2LE,	  AJA_FOURCC('N','V','1','2'),	   10,	AJAScaleTypeNone,	  false,	"10 Bit YCbCr 420 Unpacked 2-Plane LE"},	// from FFmpeg
+	{AJA_PixelFormat_YCBCR10_422PL2LE,	  AJA_FOURCC('N','V','1','6'),	   10,	AJAScaleTypeNone,	  false,	"10 Bit YCbCr 422 Unpacked 2-Plane LE"},	// from FFmpeg
 };
 const size_t AJAPixelFormatTableSize = sizeof(AJAPixelFormatTable) / sizeof(AJAPixelFormatEntry);
 
