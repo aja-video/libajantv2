a2e1b71acca2b6ba8acb68d7214fff8614daf366
MrBill <bills@aja.com>
2022-09-20 20:40:35 -0700

    Issue #844: AJAPersistence debug logging improvements: New class methods to focus read/write logging on certain keys; Read/write log messages now include prefs file, boardID, serial#; Warnings now show up in log as warnings, not notices; Renamed member vars to be more obvious that they're member vars

--- ajalibraries/ajabase/persistence/persistence.h
+++ ajalibraries/ajabase/persistence/persistence.h
@@ -6,61 +6,73 @@
 **/
 
 #ifndef AJAPersistence_H
-#define AJAPersistence_H
+	#define AJAPersistence_H
 
-#include <string>
-#include <vector>
-#include "ajabase/system/info.h"
+	#include <string>
+	#include <vector>
+	#include "ajabase/system/info.h"
 
-enum AJAPersistenceType
-{
-	AJAPersistenceTypeInt,		
-	AJAPersistenceTypeBool,
-	AJAPersistenceTypeDouble,
-	AJAPersistenceTypeString,	//std::string not C string
-	AJAPersistenceTypeBlob,
+
+	/**
+	 * Persistence data types.
+	 */
+	enum AJAPersistenceType
+	{
+		AJAPersistenceTypeInt,		
+		AJAPersistenceTypeBool,
+		AJAPersistenceTypeDouble,
+		AJAPersistenceTypeString,	//	std::string (not C string)
+		AJAPersistenceTypeBlob,
 	
-	//add any new ones above here
-	AJAPersistenceTypeEnd
-};
+		//	Add any new ones above here
+		AJAPersistenceTypeEnd
+	};
 
-/**
- * Class used to talk to the board in such a way as to maintain a persistant state 
- * across apps and reboots.
- */
-class AJAPersistence
-{
-public: 
-	AJAPersistence();
-	AJAPersistence(const std::string& appID, const std::string& deviceType="", const std::string& deviceNumber="", bool bSharePrefFile=false);
-		
-	virtual ~AJAPersistence();
 
-	void SetParams(const std::string& appID="", const std::string& deviceType="", const std::string& deviceNumber="", bool bSharePrefFile=false);
-	void GetParams(std::string& appID, std::string& deviceType, std::string& deviceNumber, bool& bSharePrefFile);
+	/**
+	 * Class used to talk to the board in such a way as to maintain a persistant state 
+	 * across apps and reboots.
+	 */
+	class AJAPersistence
+	{
+		public: 
+			AJAPersistence();
+			AJAPersistence(const std::string& appID, const std::string& deviceType="", const std::string& deviceNumber="", bool bSharePrefFile=false);
+			virtual ~AJAPersistence();
 
-	bool SetValue(const std::string& key, const void *value, AJAPersistenceType type, size_t blobBytes = 0);
-	bool GetValue(const std::string& key, void *value, AJAPersistenceType type, size_t blobBytes = 0);
-	bool FileExists();
-	bool ClearPrefFile();
-	bool DeletePrefFile(bool makeBackup = false);
-	bool StorageHealthCheck(int& errCode, std::string& errMessage);
+			void SetParams(const std::string& appID="", const std::string& deviceType="", const std::string& deviceNumber="", bool bSharePrefFile=false);
+			void GetParams(std::string& appID, std::string& deviceType, std::string& deviceNumber, bool& bSharePrefFile);
 
-	bool GetValuesInt(const std::string& keyQuery, std::vector<std::string>& keys, std::vector<int>& values);
-	bool GetValuesBool(const std::string& keyQuery, std::vector<std::string>& keys, std::vector<bool>& values);
-	bool GetValuesDouble(const std::string& keyQuery, std::vector<std::string>& keys, std::vector<double>& values);
-	bool GetValuesString(const std::string& keyQuery, std::vector<std::string>& keys, std::vector<std::string>& values);
-	
-	void PathToPrefFile(std::string& path);
-private:	
+			bool SetValue(const std::string& key, const void *value, AJAPersistenceType type, size_t blobBytes = 0);
+			bool GetValue(const std::string& key, void *value, AJAPersistenceType type, size_t blobBytes = 0);
+			bool FileExists();
+			bool ClearPrefFile();
+			bool DeletePrefFile(bool makeBackup = false);
+			bool StorageHealthCheck(int& errCode, std::string& errMessage);
 
-	std::string				mappId;
-	std::string				mboardId;
-	bool					mSharedPrefFile;
-	std::string				mserialNumber;	
-	std::string				mstateKeyName;
+			bool GetValuesInt(const std::string& keyQuery, std::vector<std::string>& keys, std::vector<int>& values);
+			bool GetValuesBool(const std::string& keyQuery, std::vector<std::string>& keys, std::vector<bool>& values);
+			bool GetValuesDouble(const std::string& keyQuery, std::vector<std::string>& keys, std::vector<double>& values);
+			bool GetValuesString(const std::string& keyQuery, std::vector<std::string>& keys, std::vector<std::string>& values);
 
-	AJASystemInfo			mSysInfo;
-};
+			void PathToPrefFile(std::string& path);
+
+		public:
+			//	Use these functions to focus read/write logging on specific keys
+			static bool DebugLogAddKey		(const std::string & inKey);
+			static bool	DebugLogHasKeys		(void);
+			static bool	DebugLogHasKey		(const std::string & inKey);
+			static bool	DebugLogRemoveKey	(const std::string & inKey);
+			static bool	DebugLogRemoveAll	(void);
+
+		private:
+			std::string		mAppID;
+			std::string		mBoardID;
+			bool			mSharedPrefFile;
+			std::string		mSerialNumber;	
+			std::string		mStateKeyName;
+			AJASystemInfo	mSysInfo;
+
+	};	//	AJAPersistence
 
 #endif	//	AJAPersistence_H
