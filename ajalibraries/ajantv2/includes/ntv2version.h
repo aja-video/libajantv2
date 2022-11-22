76c5832326e5bfc5973f079e7fd7bc75fc74b5f8
unknown <paulh@aja.com>
2022-11-22 12:13:36 -0800

    cmake: Revert build break due to checking in CMake-modified ntv2version.h

--- ajalibraries/ajantv2/includes/ntv2version.h
+++ ajalibraries/ajantv2/includes/ntv2version.h
@@ -1,24 +1,28 @@
 /* SPDX-License-Identifier: MIT */
 /**
-	@file		ntv2version.h.in
-	@brief		CMake template for generating ajantv2/includes/ntv2version.h, which populates the SDK version number.
-    These variables are set in cmake/AJAVersionConfig.cmake in the base of this repo.
+	@file		ntv2version.h
+	@brief		Defines for the NTV2 SDK version number, used by `ajantv2/includes/ntv2enums.h`.
+	See the `ajantv2/includes/ntv2version.h.in` template when building with with CMake.
 	@copyright	(C) 2013-2022 AJA Video Systems, Inc.  All rights reserved.
 **/
-
 #ifndef _NTV2VERSION_H_
 #define _NTV2VERSION_H_
 
 #include "ajaexport.h"
 
-#include <string>
+//	Automated builds remove this and the following several lines		//	__AUTO_BUILD_REMOVE__
+#if !defined (AJA_PATCHED)												//	__AUTO_BUILD_REMOVE__
+	#define AJA_DECIMAL_PLACEHOLDER			0							//	__AUTO_BUILD_REMOVE__
+	#define AJA_DATETIME_PLACEHOLDER		"00/00/0000 +8:00:00:00"	//	__AUTO_BUILD_REMOVE__
+	#define AJA_STRING_PLACEHOLDER			"d"							//	__AUTO_BUILD_REMOVE__
+#endif																	//	__AUTO_BUILD_REMOVE__
 
-#define AJA_NTV2_SDK_VERSION_MAJOR		17		///< @brief The SDK major version number, an unsigned decimal integer.
-#define AJA_NTV2_SDK_VERSION_MINOR		0		///< @brief The SDK minor version number, an unsigned decimal integer.
-#define AJA_NTV2_SDK_VERSION_POINT		0		///< @brief The SDK "point" release version, an unsigned decimal integer.
-#define AJA_NTV2_SDK_BUILD_NUMBER		1			///< @brief The SDK build number, an unsigned decimal integer.
-#define AJA_NTV2_SDK_BUILD_DATETIME		"11/21/2022 +8:21:14:37"		///< @brief The date and time the SDK was built, in this format: "MM/DD/YYYY +8:hh:mm:ss"
-#define AJA_NTV2_SDK_BUILD_TYPE			"d"			///< @brief The SDK build type, where "a"=alpha, "b"=beta, "d"=development, ""=release.
+#define AJA_NTV2_SDK_VERSION_MAJOR		AJA_DECIMAL_PLACEHOLDER			///< @brief The SDK major version number, an unsigned decimal integer.
+#define AJA_NTV2_SDK_VERSION_MINOR		AJA_DECIMAL_PLACEHOLDER			///< @brief The SDK minor version number, an unsigned decimal integer.
+#define AJA_NTV2_SDK_VERSION_POINT		AJA_DECIMAL_PLACEHOLDER			///< @brief The SDK "point" release version, an unsigned decimal integer.
+#define AJA_NTV2_SDK_BUILD_NUMBER		AJA_DECIMAL_PLACEHOLDER			///< @brief The SDK build number, an unsigned decimal integer.
+#define AJA_NTV2_SDK_BUILD_DATETIME		AJA_DATETIME_PLACEHOLDER		///< @brief The date and time the SDK was built, in this format: "MM/DD/YYYY +8:hh:mm:ss"
+#define AJA_NTV2_SDK_BUILD_TYPE			AJA_STRING_PLACEHOLDER			///< @brief The SDK build type, where "a"=alpha, "b"=beta, "d"=development, ""=release.
 
 #define AJA_NTV2_SDK_VERSION	((AJA_NTV2_SDK_VERSION_MAJOR << 24) | (AJA_NTV2_SDK_VERSION_MINOR << 16) | (AJA_NTV2_SDK_VERSION_POINT << 8) | (AJA_NTV2_SDK_BUILD_NUMBER))
 #define AJA_NTV2_SDK_VERSION_AT_LEAST(__a__,__b__)		(AJA_NTV2_SDK_VERSION >= (((__a__) << 24) | ((__b__) << 16)))
