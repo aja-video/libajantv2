aja-video/libajantv2

commit ef91732d7118cc793f8274d2ffc382808203e4a5

Issue #994: Fixed AutoCirculateInitFor… functions to trap and log illegal audio system values

ajalibraries/ajantv2/src/ntv2autocirculate.cpp
@@ -664,6 +664,9 @@ bool CNTV2Card::AutoCirculateInitForInput ( const NTV2Channel		inChannel,
 		{ACFAIL("Input Ch" << DEC(inChannel+1) << ": Frames " << DEC(startFrameNumber) << "-" << DEC(endFrameNumber) << " < 2 frames"); return false;}
 	if (inOptionFlags & (AUTOCIRCULATE_WITH_MULTILINK_AUDIO1 | AUTOCIRCULATE_WITH_MULTILINK_AUDIO2 | AUTOCIRCULATE_WITH_MULTILINK_AUDIO3)  &&  !::NTV2DeviceCanDoMultiLinkAudio(GetDeviceID()))
 		ACWARN("Input Ch" << DEC(inChannel+1) << ": MultiLink Audio requested, but device doesn't support it");
+	const UWord numAudSystems(::NTV2DeviceGetNumAudioSystems(GetDeviceID()));	//	AutoCirc cannot use AudioMixer or HostAudio
+	if (inAudioSystem  &&  UWord(inAudioSystem) >= numAudSystems)
+		{ACFAIL("Invalid audio system specified: AudSys" << DEC(inAudioSystem+1) << " -- exceeds max legal AudSys" << DEC(numAudSystems)); return false;}
 
 	//	Fill in our OS independent data structure...
 	AUTOCIRCULATE_DATA	autoCircData(eInitAutoCirc);
@@ -786,6 +789,9 @@ bool CNTV2Card::AutoCirculateInitForOutput (const NTV2Channel		inChannel,
 		{ACFAIL("Output Ch" << DEC(inChannel+1) << ": Frames " << DEC(startFrameNumber) << "-" << DEC(endFrameNumber) << " < 2 frames"); return false;}
 	if (inOptionFlags & (AUTOCIRCULATE_WITH_MULTILINK_AUDIO1 | AUTOCIRCULATE_WITH_MULTILINK_AUDIO2 | AUTOCIRCULATE_WITH_MULTILINK_AUDIO3)  &&  !::NTV2DeviceCanDoMultiLinkAudio(GetDeviceID()))
 		ACWARN("Output Ch" << DEC(inChannel+1) << ": MultiLink Audio requested, but device doesn't support it");
+	const UWord numAudSystems(::NTV2DeviceGetNumAudioSystems(GetDeviceID()));	//	AutoCirc cannot use AudioMixer or HostAudio
+	if (inAudioSystem  &&  UWord(inAudioSystem) >= numAudSystems)
+		{ACFAIL("Invalid audio system specified: AudSys" << DEC(inAudioSystem+1) << " -- exceeds max legal AudSys" << DEC(numAudSystems)); return false;}
 
 	//	Warn about "with anc" and VANC mode...
 	if (inOptionFlags & AUTOCIRCULATE_WITH_ANC)

commit 031653bef17b999f6735ad6e9879eb3029b23988

ajantv2: Fix a regression in AutoCirculateInitForInput/Output
The regression was caused by:
ef91732d7118cc793f8274d2ffc382808203e4a5
7b727e24a8b74ed2e2782f8020df85c7291a7fe2

The above commits caused the passing of NTV2_AUDIOSYSTEM_INVALID for the
audioSystem to no longer work, the passing of NTV2_AUDIOSYSTEM_INVALID
is used as a flag to init AutoCirculate without audio.

The fix is to only do the extra audioSystem checks if the passed in
audioSystem is not NTV2_AUDIOSYSTEM_INVALID.

This was the cause of issue ntv2/ntv2#1042, in After Effects we do not
use audio with AutoCirculate so the initialization was failing, which
results in no video output.

ajalibraries/ajantv2/src/ntv2autocirculate.cpp
@@ -665,8 +665,11 @@ bool CNTV2Card::AutoCirculateInitForInput ( const NTV2Channel		inChannel,
 	if (inOptionFlags & (AUTOCIRCULATE_WITH_MULTILINK_AUDIO1 | AUTOCIRCULATE_WITH_MULTILINK_AUDIO2 | AUTOCIRCULATE_WITH_MULTILINK_AUDIO3)  &&  !::NTV2DeviceCanDoMultiLinkAudio(GetDeviceID()))
 		ACWARN("Input Ch" << DEC(inChannel+1) << ": MultiLink Audio requested, but device doesn't support it");
 	const UWord numAudSystems(::NTV2DeviceGetNumAudioSystems(GetDeviceID()));	//	AutoCirc cannot use AudioMixer or HostAudio
-	if (numAudSystems  &&  UWord(inAudioSystem) >= numAudSystems)
-		{ACFAIL("Invalid audio system specified: AudSys" << DEC(inAudioSystem+1) << " -- exceeds max legal AudSys" << DEC(numAudSystems)); return false;}
+	if (inAudioSystem != NTV2_AUDIOSYSTEM_INVALID)
+	{
+		if (numAudSystems  &&  UWord(inAudioSystem) >= numAudSystems)
+			{ACFAIL("Invalid audio system specified: AudSys" << DEC(inAudioSystem+1) << " -- exceeds max legal AudSys" << DEC(numAudSystems)); return false;}
+	}
 
 	//	Fill in our OS independent data structure...
 	AUTOCIRCULATE_DATA	autoCircData(eInitAutoCirc);
@@ -790,8 +793,11 @@ bool CNTV2Card::AutoCirculateInitForOutput (const NTV2Channel		inChannel,
 	if (inOptionFlags & (AUTOCIRCULATE_WITH_MULTILINK_AUDIO1 | AUTOCIRCULATE_WITH_MULTILINK_AUDIO2 | AUTOCIRCULATE_WITH_MULTILINK_AUDIO3)  &&  !::NTV2DeviceCanDoMultiLinkAudio(GetDeviceID()))
 		ACWARN("Output Ch" << DEC(inChannel+1) << ": MultiLink Audio requested, but device doesn't support it");
 	const UWord numAudSystems(::NTV2DeviceGetNumAudioSystems(GetDeviceID()));	//	AutoCirc cannot use AudioMixer or HostAudio
-	if (numAudSystems  &&  UWord(inAudioSystem) >= numAudSystems)
-		{ACFAIL("Invalid audio system specified: AudSys" << DEC(inAudioSystem+1) << " -- exceeds max legal AudSys" << DEC(numAudSystems)); return false;}
+	if (inAudioSystem != NTV2_AUDIOSYSTEM_INVALID)
+	{
+		if (numAudSystems  &&  UWord(inAudioSystem) >= numAudSystems)
+			{ACFAIL("Invalid audio system specified: AudSys" << DEC(inAudioSystem+1) << " -- exceeds max legal AudSys" << DEC(numAudSystems)); return false;}
+	}
 
 	//	Warn about "with anc" and VANC mode...
 	if (inOptionFlags & AUTOCIRCULATE_WITH_ANC)