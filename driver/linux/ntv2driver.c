1e990372d6f4b3415e5c228f4200899ead9f583a
Sean Long <seanl@aja.com>
2023-03-28 09:21:03 -0700

    linux_driver: change needed to compile against the 6.2 kernel
    With kernel 6.2 the first parameter of dev_uevent() is now a "const
    struct" instead of a "struct".
    
    The kernel change is at:
    https://github.com/torvalds/linux/commit/23680f0b7d7f67a935adb38058110d2d81bbe6ea

--- driver/linux/ntv2driver.c
+++ driver/linux/ntv2driver.c
@@ -2839,7 +2839,12 @@ static int reboot_handler(struct notifier_block *this, unsigned long code, void 
 static UWord deviceNumber;
 
 #if defined(AJA_CREATE_DEVICE_NODES)
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6,2,0))
+static int aja_ntv2_dev_uevent(const struct device *dev, struct kobj_uevent_env *env)
+#else
 static int aja_ntv2_dev_uevent(struct device *dev, struct kobj_uevent_env *env)
+#endif
 {
 	add_uevent_var(env, "DEVMODE=%#o", 0666);
 	return 0;
