445fa2731d93537c44a8e2268fae1b0eaa18ef40
stans <stans@aja.com>
2023-07-25 16:45:24 -0600

    Add support for mem allocation in apps

--- ajabase/system/debugshare.h
+++ ajabase/system/debugshare.h
@@ -102,6 +102,7 @@ typedef enum _AJADebugUnit
 	AJA_DebugUnit_RPCServer					= 56,
 	AJA_DebugUnit_RPCClient					= 57,
 	AJA_DebugUnit_Firmware					= 58,
+	AJA_DebugUnit_App_Alloc					= 59,
 
 	// to add a new unit:
 	//
@@ -120,9 +121,8 @@ typedef enum _AJADebugUnit
 	// if no more unused units
 	//	 * set AJA_DebugUnit_FirstUnused to the same value as AJA_DebugUnit_Size
 	//
-	AJA_DebugUnit_FirstUnused				= 59,
-	AJA_DebugUnit_Unused_59					= AJA_DebugUnit_FirstUnused,
-	AJA_DebugUnit_Unused_60					= 60,
+	AJA_DebugUnit_FirstUnused				= 60,
+	AJA_DebugUnit_Unused_60					= AJA_DebugUnit_FirstUnused,
 	AJA_DebugUnit_Unused_61					= 61,
 	AJA_DebugUnit_Unused_62					= 62,
 	AJA_DebugUnit_Unused_63					= 63,
