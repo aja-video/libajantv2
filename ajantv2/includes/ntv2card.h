8ce6a140ba7a32f4302e940d5f7f245b415398e2
MrBill <bills@aja.com>
2023-10-11 15:28:26 -0700

    SDK: added CNTV2Card::GetTransmitSDIs function Also GetSDITransmitEnable & SetSDITransmitEnable now check for legit SDI connector; Marked new CNTV2Card functions as "new in SDK 17.0"; Tweaked docs for EnableBOBAnalogAudioIn

--- ajantv2/includes/ntv2card.h
+++ ajantv2/includes/ntv2card.h
@@ -2484,12 +2484,12 @@ public:
 	
 	/**
 		@brief		Enables BOB analog audio XLR inputs.
-		@param[in]	inEnable		If true, specifies that the connectors are enabled.
-									If false, specifies audio from BOB is AES.
+		@param[in]	inEnable		If true, specifies that the XLR connectors are enabled.
+									If false, breakout board audio reverts to digital AES inputs.
 		@return		True if successful;	 otherwise false.
 		@see		CNTV2Card::EnableBOBAnalogAudioIn
 	**/
-	AJA_VIRTUAL bool		EnableBOBAnalogAudioIn (bool inEnable);
+	AJA_VIRTUAL bool		EnableBOBAnalogAudioIn (bool inEnable);	//	New in SDK 17.0
 
 	/**
 		@return		True if the running device firmware supports audio start delay-til-VBI.
@@ -5277,6 +5277,17 @@ public:
 		@see		::NTV2DeviceHasBiDirectionalSDI, \ref devicesignalinputsoutputs
 	**/
 	AJA_VIRTUAL bool		GetSDITransmitEnable (const NTV2Channel inChannel, bool & outEnabled);
+
+	/**
+		@brief		Answers with the transmitting/output SDI connectors.
+		@param[out] outXmitSDIs		Receives the set of transmitting SDI connectors.
+		@return		True if successful; otherwise false.
+		@note		This function returns valid results for devices with or without bi-directional SDIs.
+		@note		This function does not take into account an SDI output that's designated as a "monitor"
+					(i.e. ::NTV2WidgetType_SDIMonOut or ::NTV2_WgtSDIMonOut1).
+		@see		::NTV2DeviceHasBiDirectionalSDI, \ref devicesignalinputsoutputs
+	**/
+	AJA_VIRTUAL bool		GetTransmitSDIs (NTV2ChannelSet & outXmitSDIs);	//	New in SDK 17.0
 	///@}
 
 	AJA_VIRTUAL bool		SetSDIOut2Kx1080Enable (const NTV2Channel inChannel, const bool inIsEnabled);
@@ -6371,7 +6382,7 @@ public:
 	AJA_VIRTUAL bool IsMultiRasterWidgetChannel (const NTV2Channel inChannel);	//	New in SDK 16.2
 	///@}
 	
-	AJA_VIRTUAL bool IsBreakoutBoardConnected (void);
+	AJA_VIRTUAL bool IsBreakoutBoardConnected (void);	//	New in SDK 17.0
 
 #if !defined(NTV2_DEPRECATE_16_1)
 	AJA_VIRTUAL inline NTV2_DEPRECATED_f(bool SetAudioOutputMonitorSource (const NTV2AudioMonitorSelect inChannelPair, const NTV2Channel inAudioSystem = NTV2_CHANNEL1))	{return SetAudioOutputMonitorSource(inChannelPair, NTV2AudioSystem(inAudioSystem));}	///< @deprecated	Use the function that uses NTV2AudioChannelPair and NTV2AudioSystem params.
