30cbeb472ada874d3d7141126272c78e80c83b33
Jim Goodnow II <jimg@aja.com>
2023-11-14 22:38:48 -0800

    another network adjustment for windows

--- ajabase/network/network.cpp
+++ ajabase/network/network.cpp
@@ -1,8 +1,8 @@
 /* SPDX-License-Identifier: MIT */
 /**
-	@file		ip_socket.cpp
-	@brief		Implements the AJAIPSocket class.
-	@copyright	(C) 2011-2022 AJA Video Systems, Inc.  All rights reserved.
+	@file		network.cpp
+	@brief		Implements the AJANetwork class.
+	@copyright	(C) 2011-2023 AJA Video Systems, Inc.  All rights reserved.
 **/
 
 /////////////////////////////
@@ -18,29 +18,10 @@ AJANetwork::~AJANetwork(void)
 {
 }
 
-int AJANetwork::aja_inet_aton(const char *cp, struct in_addr *inp)
-{
-#if !defined(AJA_BAREMETAL)
-  return inet_aton(cp, inp);
-#else
-  return 0;
-#endif
-}
-
 in_addr_t AJANetwork::aja_inet_addr(const char *cp)
 {
 #if !defined(AJA_BAREMETAL)
   return inet_addr(cp);
-#else
-  in_addr_t t;
-  return t;
-#endif
-}
-
-in_addr_t AJANetwork::aja_inet_network(const char *cp)
-{
-#if !defined(AJA_BAREMETAL)
-  return inet_network(cp);
 #else
   in_addr_t t;
   return t;
@@ -53,36 +34,6 @@ char *AJANetwork::aja_inet_ntoa(struct in_addr in)
   return inet_ntoa(in);
 #else
   return "";
-#endif
-}
-
-struct in_addr AJANetwork::aja_inet_makeaddr(int net, int host)
-{
-#if !defined(AJA_BAREMETAL)
-  return inet_makeaddr(net, host);
-#else
-  struct in_addr t;
-  return t;
-#endif
-}
-
-in_addr_t AJANetwork::aja_inet_lnaof(struct in_addr in)
-{
-#if !defined(AJA_BAREMETAL)
-  return inet_lnaof(in);
-#else
-  in_addr_t t;
-  return t;
-#endif
-}
-
-in_addr_t AJANetwork::aja_inet_netof(struct in_addr in)
-{
-#if !defined(AJA_BAREMETAL)
-  return inet_netof(in);
-#else
-  in_addr_t t;
-  return t;
 #endif
 }
 
--- ajabase/network/network.h
+++ ajabase/network/network.h
@@ -58,13 +58,8 @@ class AJA_EXPORT AJANetwork
 		AJANetwork(void);
 		virtual ~AJANetwork(void);
 
-    static int aja_inet_aton(const char *cp, struct in_addr *inp);
     static in_addr_t aja_inet_addr(const char *cp);
-    static in_addr_t aja_inet_network(const char *cp);
     static char *aja_inet_ntoa(struct in_addr in);
-    static struct in_addr aja_inet_makeaddr(int net, int host);
-    static in_addr_t aja_inet_lnaof(struct in_addr in);
-    static in_addr_t aja_inet_netof(struct in_addr in);
 };
 
 
