e400af18eef7b4535331cccbfe6381eb582f3fc7
Ian Stewart <istewart@nvidia.com>
2024-05-09 12:01:10 -0600

    Fix nv-p2p function signatures for iGPU
    Fixes an rdma_get_pages argument type error, and
    moves rdma_put_pages for consistency.

--- driver/linux/ntv2rdma.h
+++ driver/linux/ntv2rdma.h
@@ -11,24 +11,17 @@
 #ifdef AJA_RDMA
 #include <nv-p2p.h>
 
+typedef int (*rdma_free_page_table)(struct nvidia_p2p_page_table *page_table);
+
+#ifdef AJA_IGPU
+
 typedef int (*rdma_get_pages)(
-    uint64_t p2p_token, uint32_t va_space,
-#ifndef AJA_IGPU
-    uint64_t virtual_address, uint64_t length,
-#endif                              
+    uint64_t vaddr, uint64_t size,
     struct nvidia_p2p_page_table **page_table,
     void (*free_callback)(void *data), void *data);
 
 typedef int (*rdma_put_pages)(
-#ifndef AJA_IGPU
-    uint64_t p2p_token,
-    uint32_t va_space, uint64_t virtual_address,
-#endif    
     struct nvidia_p2p_page_table *page_table);
-
-typedef int (*rdma_free_page_table)(struct nvidia_p2p_page_table *page_table);
-
-#ifdef AJA_IGPU
 
 typedef int (*rdma_dma_map_pages)(
     struct device *dev,
@@ -40,6 +33,17 @@ typedef int (*rdma_dma_unmap_pages)(
     struct nvidia_p2p_dma_mapping *dma_mapping);
 
 #else
+
+typedef int (*rdma_get_pages)(
+    uint64_t p2p_token, uint32_t va_space,
+    uint64_t virtual_address, uint64_t length,
+    struct nvidia_p2p_page_table **page_table,
+    void (*free_callback)(void *data), void *data);
+
+typedef int (*rdma_put_pages)(
+    uint64_t p2p_token,
+    uint32_t va_space, uint64_t virtual_address,
+    struct nvidia_p2p_page_table *page_table);
 
 typedef int (*rdma_dma_map_pages)(
     struct pci_dev *peer,
