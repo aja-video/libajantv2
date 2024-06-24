aja-video/libajantv2

commit 8d285a0ca3d49492da9078efb86b2aab74f96b42

CNTV2DriverInterface::OpenRemote update: • For numeric deviceSpecs, directly opens local device without temporarily opening 2nd card instance

ajantv2/src/ntv2driverinterface.cpp
@@ -258,17 +258,23 @@ bool CNTV2DriverInterface::OpenRemote (const string & inURLSpec)
 		{DIFAIL("Bad device specification '" << inURLSpec << "': " << specParser.Error()); return false;}
 
 	if (specParser.IsLocalDevice())
-	{	//	Local device?
+	{	//	Local device...
 		CNTV2Card card;
 		if (specParser.HasResult(kConnectParamDevSerial))
-			CNTV2DeviceScanner::GetDeviceWithSerial(specParser.DeviceSerial(), card);
+		{	if (CNTV2DeviceScanner::GetDeviceWithSerial(specParser.DeviceSerial(), card))
+				Open(card.GetIndexNumber());
+		}
 		else if (specParser.HasResult(kConnectParamDevModel))
-			CNTV2DeviceScanner::GetFirstDeviceWithName(specParser.DeviceModel(), card);
+		{	if (CNTV2DeviceScanner::GetFirstDeviceWithName(specParser.DeviceModel(), card))
+				Open(card.GetIndexNumber());
+		}
 		else if (specParser.HasResult(kConnectParamDevID))
-			CNTV2DeviceScanner::GetFirstDeviceWithID(specParser.DeviceID(), card);
+		{	if (CNTV2DeviceScanner::GetFirstDeviceWithID(specParser.DeviceID(), card))
+				Open(card.GetIndexNumber());
+		}
 		else if (specParser.HasResult(kConnectParamDevIndex))
-			CNTV2DeviceScanner::GetDeviceAtIndex(specParser.DeviceIndex(), card);
-		if (!card.IsOpen())
+			Open(specParser.DeviceIndex());
+		if (!IsOpen())
 			{DIFAIL("Failed to open " << specParser.InfoString());  return false;}
 		return Open(card.GetIndexNumber());
 	}

commit 6202f51265d3e448328f05e608e5cbdf51ab362e

Fixed bug introduced in commit 8d285a0c CNTV2DriverInterface::OpenRemote always opened device 0 for local deviceSpecs that identified any device other than device 0

ajantv2/src/ntv2driverinterface.cpp
@@ -276,7 +276,7 @@ bool CNTV2DriverInterface::OpenRemote (const string & inURLSpec)
 			Open(specParser.DeviceIndex());
 		if (!IsOpen())
 			{DIFAIL("Failed to open " << specParser.InfoString());  return false;}
-		return Open(card.GetIndexNumber());
+		return true;
 	}
 #if defined(NTV2_NUB_CLIENT_SUPPORT)
 	DIDBG("Opening " << specParser.InfoString());