d311cc27b072d02b3dbd335647e48f62345e33fa
MrBill <bills@aja.com>
2026-02-19 16:51:07 -0800

    Issue sw/ntv2/top#1231: 'sdram' param is now 'fbinit'

--- ajantv2/src/ntv2nubaccess.cpp
+++ ajantv2/src/ntv2nubaccess.cpp
@@ -435,7 +435,7 @@ void NTV2DeviceSpecParser::Parse (void)
 			//	"xxxx://swdevice/?"
 			//		"nosharedmemory"
 			//		"&supportlog=file%3A%2F%2F%2FUsers%2Fdemo%2FDesktop%2FAJAWatcherSupport.log"
-			//		"&sdram=file%3A%2F%2F%2FUsers%2Fdemo%2FDesktop%2FSDRAMsnapshot.dat");
+			//		"&fbinit=file%3A%2F%2F%2FUsers%2Fdemo%2FDesktop%2FSDRAMsnapshot.dat");
 			//	Host[port]/[resource[?query]]
 			size_t posURL(posScheme), posRsrc(0);
 			string	host, port, rsrcPath;
@@ -1024,7 +1024,7 @@ bool NTV2DeviceSpecParser::IsLegalSerialNumChar (const char inChar)
 		specParser.Reset("ntv2://swdevice/?"
 							"nosharedmemory"
 							"&supportlog=file%3A%2F%2F%2FUsers%2Fdemo%2FDesktop%2FAJAWatcherSupport.log"
-							"&sdram=file%3A%2F%2F%2FUsers%2Fdemo%2FDesktop%2FSDRAMsnapshot.dat");
+							"&fbinit=file%3A%2F%2F%2FUsers%2Fdemo%2FDesktop%2FSDRAMsnapshot.dat");
 	}
 #endif	//	defined(_DEBUG)
 
